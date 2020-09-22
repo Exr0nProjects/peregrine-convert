815b2b3b3eaed53ae356c466fe9c0795c15e47ff
Exr0n <spotyie@gmail.com>
2020-09-21 22:41:57 -0700

    what is even breaking

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #define isdigit(x) (x >= '0' && x <= '9')
 
@@ -26,70 +27,135 @@ inline val_t scan(char **p)
 }
 
 // with github.com/richardfeynmanrocks
+//void prep()
+//{
+//    int num_cnt = 0;
+//    FILE* wptr = fopen("compressed.bin", "wb");
+//    static const auto BUFFER_SIZE = 16*1024;
+//    int fd = open("data.csv", O_RDONLY | O_NONBLOCK);
+//    if (fd == -1) {
+//        printf("fd == -1\n");
+//    }
+//    val_t tmp;
+//    char buf[BUFFER_SIZE + 1];
+//    while (size_t bytes_read = read(fd, buf, BUFFER_SIZE))
+//    {
+//        if (bytes_read == (size_t)-1) {
+//            printf("bytes_read == (size_t)-1\n");
+//        }
+//        if (!bytes_read) break;
+//        for (char *p = buf;;) {
+//            char* bound = (char*) memchr(p, '\n', (buf + bytes_read) - p);
+//            if (bound - p < 0) break; // Stop.
+//            for (int i=0; i<5; ++i) {
+//                tmp = scan(&p);
+//                proc[num_cnt++ % DEBUG_BUF_SIZE] = tmp;
+//                fwrite((void*)&tmp, sizeof(val_t), 1, wptr);
+//            }
+//            p = bound + 1;
+//        }
+//    }
+//}
 void prep()
 {
     int num_cnt = 0;
     FILE* wptr = fopen("compressed.bin", "wb");
     static const auto BUFFER_SIZE = 16*1024;
     int fd = open("data.csv", O_RDONLY | O_NONBLOCK);
-    if (fd == -1) {
+    if(fd == -1) {
         printf("fd == -1\n");
     }
-    val_t tmp;
+    float tmp;
     char buf[BUFFER_SIZE + 1];
-    while (size_t bytes_read = read(fd, buf, BUFFER_SIZE))
+    while(size_t bytes_read = read(fd, buf, BUFFER_SIZE))
     {
-        if (bytes_read == (size_t)-1) {
+        if(bytes_read == (size_t)-1) {
             printf("bytes_read == (size_t)-1\n");
         }
         if (!bytes_read) break;
-        for (char *p = buf;;) {
+        for(char *p = buf;;) {
             char* bound = (char*) memchr(p, '\n', (buf + bytes_read) - p);
             if (bound - p < 0) break; // Stop.
             for (int i=0; i<5; ++i) {
                 tmp = scan(&p);
                 proc[num_cnt++ % DEBUG_BUF_SIZE] = tmp;
-                fwrite((void*)&tmp, sizeof(val_t), 1, wptr);
+                fwrite((void*)&tmp, sizeof(float), 1, wptr);
             }
             p = bound + 1;
+            putc('\n', wptr);
         }
     }
 }
-void read()
+float read()
 {
     int num_cnt = 0;
-    static const auto BUFFER_SIZE = 16*1024;
-    int fd = open("compressed.bin", O_RDONLY | O_NONBLOCK);
-    if (fd == -1) {
+    static const auto BUFFER_SIZE = 1024*1024;
+    int fd = open("data.csv", O_RDONLY | O_NONBLOCK);
+    if(fd == -1) {
         printf("fd == -1\n");
+        return 1;
     }
-    val_t tmp;
     char buf[BUFFER_SIZE + 1];
-    while (size_t bytes_read = read(fd, buf, BUFFER_SIZE))
+    uintmax_t lines = 0;
+    float tmp;
+    while(size_t bytes_read = read(fd, buf, BUFFER_SIZE))
     {
-        if (bytes_read == (size_t)-1) {
+        if(bytes_read == (size_t)-1) {
             printf("bytes_read == (size_t)-1\n");
+            return 1;
         }
-        //if (!bytes_read) break;
-        for (char *p = buf; p < buf+BUFFER_SIZE;) {
-            //char* bound = (char*) memchr(p, '\n', (buf + bytes_read) - p);
-            //if (bound - p < 0) break; // Stop.
-            //for (int i=0; i<5; ++i) {
-            tmp = *(float*)p;
-            if (proc[num_cnt++ % DEBUG_BUF_SIZE] != tmp)
-                printf("printf %3d mismatech: %f vs %f\n", num_cnt-1, proc[(num_cnt-1) % DEBUG_BUF_SIZE], tmp);
-
-            p+=sizeof(float);
-            //}
-            //p = bound + 1;
+        if (!bytes_read) break;
+        for(char *p = buf;;) {
+            char* bound = (char*) memchr(p, '\n', (buf + bytes_read) - p);
+            if (bound - p < 0) break; // Stop.
+            for (int i=0; i<5; ++i) {
+                if (proc[num_cnt++ % DEBUG_BUF_SIZE] != tmp)
+                    printf("printf %3d mismatech: %f vs %f\n", num_cnt-1, proc[(num_cnt-1) % DEBUG_BUF_SIZE], tmp);
+                tmp = *((float*)p);
+                p += sizeof(float);
+            }
+            p = bound + 1;
+            ++lines;
         }
     }
+    return tmp;
 }
+
+//void read()
+//{
+//    int num_cnt = 0;
+//    static const auto BUFFER_SIZE = 16*1024;
+//    int fd = open("compressed.bin", O_RDONLY | O_NONBLOCK);
+//    if (fd == -1) {
+//        printf("fd == -1\n");
+//    }
+//    val_t tmp;
+//    char buf[BUFFER_SIZE + 1];
+//    while (size_t bytes_read = read(fd, buf, BUFFER_SIZE))
+//    {
+//        if (bytes_read == (size_t)-1) {
+//            printf("bytes_read == (size_t)-1\n");
+//        }
+//        //if (!bytes_read) break;
+//        for (char *p = buf; p < buf+BUFFER_SIZE;) {
+//            //char* bound = (char*) memchr(p, '\n', (buf + bytes_read) - p);
+//            //if (bound - p < 0) break; // Stop.
+//            //for (int i=0; i<5; ++i) {
+//            tmp = *(float*)p;
+//            if (proc[num_cnt++ % DEBUG_BUF_SIZE] != tmp)
+//                printf("printf %3d mismatech: %f vs %f\n", num_cnt-1, proc[(num_cnt-1) % DEBUG_BUF_SIZE], tmp);
+//
+//            p+=sizeof(float);
+//            //}
+//            //p = bound + 1;
+//        }
+//    }
+//}
 
 int main()
 {
     prep();
-    scanf("%*c");
+    //scanf("%*c");
     read();
 }
 
